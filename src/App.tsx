34865e33af36ba81e29f84596aac32ea73049d26
aforanup <aforanupn@gmail.com>
2024-09-09 12:22:53 +0545

    created login and signup page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+const App = () => {
+    return (
+        <>
+            <Signup />
+            <Login />
+        </>
+    );
+};
+export default App;
